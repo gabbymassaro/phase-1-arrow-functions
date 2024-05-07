a60716bac2cadb17dc2721ae40bd58d9f8dbbf14
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-07 08:38:50 -0400

    Remove console logging

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let a;
 let b;
 let x;
@@ -14,15 +13,3 @@ square(x, x);
 const add = (a, b) => {
     return (a + b);
 }
-
-
-//--- example console logging function results ---/
-const result = divide(6, 2);
-console.log('divide:', result);
-
-
-const result2 = square(5);
-console.log('square:', result2);
-
-const result3 = add(8, 10);
-console.log('add:', result3);
